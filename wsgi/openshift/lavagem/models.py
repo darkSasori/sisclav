355f5f4e8187077faf358750780e7df29ce1ef39
Lineu Felipe <lineufelipe@gmail.com>
2013-03-10 22:33:00 -0400

    Admin

--- wsgi/openshift/lavagem/models.py
+++ wsgi/openshift/lavagem/models.py
@@ -7,4 +7,6 @@ class Venda(models.Model):
     funExterior = models.ForeignKey(User, verbose_name='Funcionario Exterior', on_delete=models.DO_NOTHING)
     carro = models.ForeignKey(Carro, verbose_name='Carro', on_delete=models.DO_NOTHING)
     categoria = models.ForeignKey(Categoria, verbose_name='Categoria', on_delete=models.DO_NOTHING)
-    entrada = models.DateTimeField(auto_now=
+    entrada = models.DateTimeField(auto_now=True, verbose_name='Entrada')
+    saida = models.DateTimeField(auto_now=True, verbose_name='Saida')
+    km = models.DecimalField(max_digits=16, decimal_places=2, verbose_name='Quilometragem')
