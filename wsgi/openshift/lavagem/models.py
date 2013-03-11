f0128e205e405e5499f5e6494a520c364824dcd1
Lineu Felipe <lineufelipe@gmail.com>
2013-03-10 22:56:07 -0400

    Problema no related_name

--- wsgi/openshift/lavagem/models.py
+++ wsgi/openshift/lavagem/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from cadastro.models import Carro, Categoria
 
 class Venda(models.Model):
-    funInterior = models.ForeignKey(User, verbose_name='Funcionario Interior', on_delete=models.DO_NOTHING)
-    funExterior = models.ForeignKey(User, verbose_name='Funcionario Exterior', on_delete=models.DO_NOTHING)
+    funInterior = models.ForeignKey(User, verbose_name='Funcionario Interior', on_delete=models.DO_NOTHING, related_name='fk_venda_user_int')
+    funExterior = models.ForeignKey(User, verbose_name='Funcionario Exterior', on_delete=models.DO_NOTHING, related_name='fk_venda_user_ext')
     carro = models.ForeignKey(Carro, verbose_name='Carro', on_delete=models.DO_NOTHING)
     categoria = models.ForeignKey(Categoria, verbose_name='Categoria', on_delete=models.DO_NOTHING)
     entrada = models.DateTimeField(auto_now=True, verbose_name='Entrada')
