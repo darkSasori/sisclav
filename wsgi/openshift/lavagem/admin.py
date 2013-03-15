b2f5d269a0d68e482246bf6c74e544aeae52da17
Lineu Felipe <lineufelipe@gmail.com>
2013-03-15 05:18:30 -0400

    Teste de raw field

--- wsgi/openshift/lavagem/admin.py
+++ wsgi/openshift/lavagem/admin.py
@@ -7,6 +7,8 @@ class InlineItem(admin.TabularInline):
 
 class AdminVenda(admin.ModelAdmin):
     inlines = [InlineItem]
+    exclude = ('status',)
+    raw_id_fields = ('carro_placa',)
     list_display = ('carro', 'categoria', 'entrada', 'saida', 'km', 'status', 'funInterior', 'funExterior',)
     search_fields = ['carro_placa',]
     list_filtes = ('status',)
